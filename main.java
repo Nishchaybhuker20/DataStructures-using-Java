f69e6e23c5e5a39e920ff4864c57af27d193288e
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-02-08 08:45:58 +0530

    Array declaration and updation

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class main{
+    public static void main(String args[]){
+        int[] marks = new int[3];
+        marks[0] = 99;
+        marks[1] = 100;
+        marks[2] = 98;
+        System.out.println(marks);
+    }
+}
