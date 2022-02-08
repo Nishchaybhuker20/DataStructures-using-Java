0a245d13e642e2f318a62561e72bf2304ef46e15
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-02-08 09:49:55 +0530

    Array declaration and updation

---
+++ Strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Strings{
+    public static void main(String args[]){
+        String firstName = "Nishchay";
+        String LastName = "bhuker";
+        String emailAddress = firstName+LastName+"@gmail.com";
+
+        System.out.println(emailAddress);
+    }
+}
