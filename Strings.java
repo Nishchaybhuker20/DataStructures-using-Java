a6e82d1370832f7fbe1dc3cc9e3a16e98e7e6789
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-02-08 09:59:31 +0530

    Array declaration and updation

--- Strings.java
+++ Strings.java
@@ -5,13 +5,19 @@ public class Strings{
         String firstName = "Nishchay";
         String LastName = "bhuker";
         String emailAddress = firstName+LastName+"@gmail.com";
+        //printing email address
 
         System.out.println(emailAddress);
 
+//Creating substrings
+        String name = firstName.substring(4, firstName.length());
+        System.out.println(name);
+
+//printing individual letters
         for(int i = 0; i<emailAddress - Strings.main(String[]){
         System.out.println(emailAddress.charAt(i));
     }
-
+//control statements
     if(firstName.compareTo(LastName) == 0){
         System.out.println("Strings are equal");
     }else{
