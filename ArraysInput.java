e540cbc0cb27d3c1e3e8d45fbdca70a32b64ddd7
NISHCHAY <76655825+Nishchaybhuker20@users.noreply.github.com>
2022-02-08 08:54:16 +0530

    Array declaration and updation

---
+++ ArraysInput.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Arrays{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        for(int i =0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i<size; i++){
+            System.out.println(numbers[i]);
+        }
+    }
+}
